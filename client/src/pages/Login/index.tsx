2e766b310d33e6ab540889aa63c4b53526e73cdc
vadim <vadimkaptsov@mail.ru>
2023-02-09 18:51:41 +0300

    [Refactor] - rm unused imports

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,8 +1,5 @@
 import './login.scss'
 
-import logo from '@Assets/images/logo-black-short.png'
-import { NavLink } from 'react-router-dom'
-
 import { LoginFooter } from './components/LoginFooter/LoginFooter'
 import { LoginForm } from './components/LoginForm/LoginForm'
 import { LoginHeader } from './components/LoginHeader/LoginHeader'
